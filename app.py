bfbe39609e42a35a02e2cfa495bb686b14a7e191
paliwalhimanshu20-droid <paliwalhimanshu20@gmail.com>
2026-06-10 23:46:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ CHAT_ID = "8351444537"
 
 UPSTOX_ACCESS_TOKEN = "eyJ0eXAiOiJKV1QiLCJrZXlfaWQiOiJza192MS4wIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiIxNzA3OTkiLCJqdGkiOiI2YTI5YTZlYWNiNzgzMjM4OWUxNzZmNGUiLCJpc011bHRpQ2xpZW50IjpmYWxzZSwiaXNQbHVzUGxhbiI6dHJ1ZSwiaWF0IjoxNzgxMTE0NjAyLCJpc3MiOiJ1ZGFwaS1nYXRld2F5LXNlcnZpY2UiLCJleHAiOjE3ODExMjg4MDB9.A26llWp4-Of-Fy7W4Np8QG2LQrFSAGwpCkMJ3mSdvn0"
 
+LAST_SIGNAL = None
 # ---------------- TELEGRAM FUNCTION ----------------
 
 def send_telegram(message):
@@ -652,7 +653,19 @@ T1: {t1}
 T2: {t2}
 """
 
+    global LAST_SIGNAL
+
+if signal_type != LAST_SIGNAL:
+
     send_telegram(message)
+
+    LAST_SIGNAL = signal_type
+
+    st.success("📨 New Signal Sent To Telegram")
+
+else:
+
+    st.info("ℹ️ Same signal already sent")
 
     st.success(f"{signal_type} SIGNAL")
 
