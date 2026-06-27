ccc7de30888f91f8cf1907416d944ad2e050fef7
paliwalhimanshu20-droid <paliwalhimanshu20@gmail.com>
2026-06-27 13:57:18 +0530

    Update test_backtest.py

--- test_backtest.py
+++ test_backtest.py
@@ -1,1 +1,8 @@
+from strategy_lab.backtest import run_backtest
 
+result = run_backtest(
+    instrument_name="TEST",
+    instrument_key="NSE_EQ|INE002A01018"   # example (you can change later)
+)
+
+print(result)
