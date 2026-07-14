f35e000c768127ec8696881b0aa52ee3b2c46db9
paliwalhimanshu20-droid <paliwalhimanshu20@gmail.com>
2026-07-15 00:05:29 +0530

    Add files via upload

--- jarvis-android/app/src/main/java/com/jarvis/os/app/core/workflow/di/WorkflowModule.kt
+++ jarvis-android/app/src/main/java/com/jarvis/os/app/core/workflow/di/WorkflowModule.kt
@@ -1,1 +1,17 @@
+package com.jarvis.os.app.core.workflow.di
 
+import com.jarvis.os.app.core.workflow.DefaultWorkflowEngine
+import com.jarvis.os.app.core.workflow.WorkflowEngine
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class WorkflowModule {
+    @Binds
+    @Singleton
+    abstract fun bindWorkflowEngine(impl: DefaultWorkflowEngine): WorkflowEngine
+}
