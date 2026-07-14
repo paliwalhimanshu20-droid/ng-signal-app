ccdc3865bb69d83931cac95a02cdbd03233e40f8
paliwalhimanshu20-droid <paliwalhimanshu20@gmail.com>
2026-07-15 00:02:44 +0530

    Add files via upload

--- jarvis-android/app/src/main/java/com/jarvis/os/app/core/security/di/SecurityModule.kt
+++ jarvis-android/app/src/main/java/com/jarvis/os/app/core/security/di/SecurityModule.kt
@@ -1,1 +1,17 @@
+package com.jarvis.os.app.core.security.di
 
+import com.jarvis.os.app.core.security.InMemorySecretVault
+import com.jarvis.os.app.core.security.SecretVault
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class SecurityModule {
+    @Binds
+    @Singleton
+    abstract fun bindSecretVault(impl: InMemorySecretVault): SecretVault
+}
