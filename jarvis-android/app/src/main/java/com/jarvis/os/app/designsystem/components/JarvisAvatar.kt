d24c5842710fe27aaaa4b153a75d7826f2d61c32
paliwalhimanshu20-droid <paliwalhimanshu20@gmail.com>
2026-07-15 21:27:29 +0530

    Add files via upload

--- jarvis-android/app/src/main/java/com/jarvis/os/app/designsystem/components/JarvisAvatar.kt
+++ jarvis-android/app/src/main/java/com/jarvis/os/app/designsystem/components/JarvisAvatar.kt
@@ -3,6 +3,7 @@ package com.jarvis.os.app.designsystem.components
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
